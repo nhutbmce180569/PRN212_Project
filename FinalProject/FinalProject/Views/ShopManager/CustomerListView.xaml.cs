42da028718c37a3be8cf1d8ffaad0538ce6a69bc
nhutbmce180569 <nhutbmce180569@fpt.edu.vn>
2025-03-14 14:59:51 +0700

    Update UI

--- FinalProject/FinalProject/Views/ShopManager/CustomerListView.xaml.cs
+++ FinalProject/FinalProject/Views/ShopManager/CustomerListView.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using FinalProject.ViewModels;
+using FinalProject.ViewModels.ShopManager;
 
 namespace FinalProject.Views.ShopManager
 {
