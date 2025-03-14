056eeaab7c754d7d52cba3383ddf99bf49d6b80a
TuongMPCE180644 <TuongMPCE180644@fpt.edu.vn>
2025-03-14 16:55:41 +0700

    add login function

--- FinalProject/FinalProject/Views/Login.xaml.cs
+++ FinalProject/FinalProject/Views/Login.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using FinalProject.ViewModels;
 using FinalProject.Views;
 
 namespace FinalProject
@@ -20,6 +21,7 @@ namespace FinalProject
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = new LoginViewModel();
         }
 
         //private void Button_Click(object sender, RoutedEventArgs e)
