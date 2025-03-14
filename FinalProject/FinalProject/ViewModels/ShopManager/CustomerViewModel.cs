de4927875203070e3354052bcee7d467a2a21012
nhutbmce180569 <nhutbmce180569@fpt.edu.vn>
2025-03-14 16:35:56 +0700

    Popup

--- FinalProject/FinalProject/ViewModels/ShopManager/CustomerViewModel.cs
+++ FinalProject/FinalProject/ViewModels/ShopManager/CustomerViewModel.cs
@@ -8,6 +8,8 @@ using System.Windows.Input;
 using FinalProject.Models;
 using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 using WPFLab.Helper;
+using FinalProject.Views.ShopManager.Customer;
+using System.Windows;
 
 namespace FinalProject.ViewModels.ShopManager
 {
@@ -15,6 +17,7 @@ namespace FinalProject.ViewModels.ShopManager
     {
         public ObservableCollection<Customer> CustomerList { get; set; }
 
+        public ICommand OpenPopupCommand { get; }
         public ICommand AddCommand { get; }
         public ICommand UpdateCommand { get; }
         public ICommand DeleteCommand { get; }
@@ -26,7 +29,40 @@ namespace FinalProject.ViewModels.ShopManager
             UpdateCommand = new RelayCommand(Update);
             DeleteCommand = new RelayCommand(Delete);
             SearchCommand = new RelayCommand(Search);
+            OpenPopupCommand = new RelayCommand(OpenPopup);
         }
+        private void OpenPopup(object obj)
+        {
+            var popup = new AddCustomer
+            {
+                Owner = Application.Current.MainWindow,
+                WindowStartupLocation = WindowStartupLocation.CenterOwner
+            };
+
+            // Dùng Dispatcher để tránh lỗi "Window is closing"
+            popup.Deactivated += (s, e) =>
+            {
+                if (popup.IsLoaded)
+                {
+
+                    Application.Current.Dispatcher.BeginInvoke(new Action(() =>
+                    {
+                        if (!popup.IsActive) // khi click chuột ra ngoài không trong popup
+                        {
+                            popup.Topmost = true;
+
+                            //// Lấy lại focus cho MainWindow
+                            //Application.Current.MainWindow.Activate();
+                            //Application.Current.MainWindow.Opacity = 1;
+
+                        }
+                    }));
+                }
+            };
+            Application.Current.MainWindow.Opacity = 0.5;
+            popup.ShowDialog();
+        }
+
 
         public void Load()
         {
